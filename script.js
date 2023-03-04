867fba12148fddd01ee1d29674c77d11696e366f
Mike Jackson <perigran_falcon@yahoo.com>
2023-03-04 14:49:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 // script.js
 
+// For the Tabbed Trucking Gallery
 function myFunction(imgs) {
     var expandImg = document.getElementById("expandedImg");
     var imgText = document.getElementById("imgtext");
     expandImg.src = imgs.src;
     imgText.innerHTML = imgs.alt;
     expandImg.parentElement.style.display = "block";
+}
+
+// For the Dinosaur Image Gallery
+var imageThumbs = document.getElementById("image-thumbs");
+var currentImage = document.getElementById("current-image");
+
+for (var i = 1; i <= 3; i++) {
+  var thumb = document.createElement("img");
+  thumb.src = "/images/dinosaur" + i + ".jpg";
+  thumb.alt = "dinosaur " + i;
+  thumb.classList.add("thumb");
+  imageThumbs.appendChild(thumb);
+  thumb.addEventListener(
+    "click", function() {
+      currentImage.src = this.src;
+    }
+  );
 }
