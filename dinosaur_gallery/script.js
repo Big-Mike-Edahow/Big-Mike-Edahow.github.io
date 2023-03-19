4c2cea6ad5154344f944659f6136c953e2f0855f
Mike Jackson <perigran_falcon@yahoo.com>
2023-03-19 16:23:18 -0600

    Create script.js

---
+++ dinosaur_gallery/script.js
@@ -0,0 +1,13 @@
+// script.js
+
+var imageThumbs = document.getElementById("image-thumbs");
+var currentImage = document.getElementById("current-image");
+
+for (var i = 1; i <= 5; i++) {
+    var thumb = document.createElement("img");
+    thumb.src = "images/dinosaur" + i + ".jpg";
+    thumb.alt = "dinosaur " + i;
+    thumb.classList.add("thumb");
+    imageThumbs.appendChild(thumb);
+    thumb.addEventListener("click", function () { currentImage.src = this.src; });
+}
